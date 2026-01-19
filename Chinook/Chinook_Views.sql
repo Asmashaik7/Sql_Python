f7873b3b865cd6bdf99e751d7b980e0e45d83b75
Asma Shaik <asmashaik.nov11@gmail.com>
2026-01-19 13:39:39 +0530

    Update Chinook_Views.sql

--- Chinook/Chinook_Views.sql
+++ Chinook/Chinook_Views.sql
@@ -1,5 +1,5 @@
---Views are like virtual tables, which can b used multiple times to avoid writing big and complex quries involving 3 to 5 table joins.
---EX: Retrive customer details and their assigned customer reps
+--Views are like virtual tables, which can b used multiple times to avoid writing big and complex queries involving 3 to 5 table joins.
+--EX: Retrive customer details and their assigned customer representatives
 
 select * 
 from Customer c
@@ -30,8 +30,9 @@ left join employee e
 on c.SupportRepId=e.EmployeeId;
 
 SELECT * from CustomerDetail;
--- here we r using view simply by giving its name, rather than writing complex queries again.
+-- here we are using view simply by giving its name, rather than writing complex queries again.
 
 select * 
 from CustomerDetail
 where SupportRepName='Jane';
+
